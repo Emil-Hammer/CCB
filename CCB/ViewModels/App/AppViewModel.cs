2bb6b0e0bef6e73a5f8198c4c606c266a391939d
Emil Hammer <mr_h4mm3r@live.dk>
2018-12-12 12:40:08 +0100

    Første brugbare version. Følgende virker; Database forbindelse CRUD på StaffView Automatisk loading af db ved start af program

--- CCB/ViewModels/App/AppViewModel.cs
+++ CCB/ViewModels/App/AppViewModel.cs
@@ -9,7 +9,7 @@ namespace CCB.ViewModels.App
     {
         public override void AddCommands()
         {
-            NavigationCommands.Add("OpenMainPage", new NavigationCommand(AppFrame, typeof(FileView)));
+            NavigationCommands.Add("OpenFrontView", new NavigationCommand(AppFrame, typeof(FrontView)));
             NavigationCommands.Add("OpenInventoryView", new NavigationCommand(AppFrame, typeof(InventoryView)));
             NavigationCommands.Add("OpenProjectView", new NavigationCommand(AppFrame, typeof(ProjectView)));
             NavigationCommands.Add("OpenStaffView", new NavigationCommand(AppFrame, typeof(StaffView)));
